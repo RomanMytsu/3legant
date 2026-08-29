c8465248e3c45d318f1c1e7c1cf8f2522e718a28
Roman <romanmytsu@gmail.com>
2026-08-29 14:15:16 +0300

    added

---
+++ src/pages/home/model/ValuesItem.ts
@@ -0,0 +1,28 @@
+import { ValueItem } from "../ui/values/values";
+
+export const VALUES: ValueItem[] = [
+  {
+    id: "shipping",
+    icon: "fast-delivery",
+    title: "Free Shipping",
+    description: "Order above $200",
+  },
+  {
+    id: "money-back",
+    icon: "money-back",
+    title: "Money-back",
+    description: "30 days guarantee",
+  },
+  {
+    id: "secure",
+    icon: "lock",
+    title: "Secure Payments",
+    description: "Secured by Stripe",
+  },
+  {
+    id: "support",
+    icon: "call",
+    title: "24/7 Support",
+    description: "Phone and Email support",
+  },
+];
--- src/pages/home/ui/values/values.tsx
+++ src/pages/home/ui/values/values.tsx
@@ -1,39 +1,13 @@
 import { Icon } from "@/shared/ui/Icon";
+import { VALUES } from "../../model/ValuesItem";
 import s from "./values.module.scss";
 
-interface ValueItem {
+export interface ValueItem {
   id: string;
   icon: string;
   title: string;
   description: string;
 }
-
-const VALUES: ValueItem[] = [
-  {
-    id: "shipping",
-    icon: "fast-delivery",
-    title: "Free Shipping",
-    description: "Order above $200",
-  },
-  {
-    id: "money-back",
-    icon: "money-back",
-    title: "Money-back",
-    description: "30 days guarantee",
-  },
-  {
-    id: "secure",
-    icon: "lock",
-    title: "Secure Payments",
-    description: "Secured by Stripe",
-  },
-  {
-    id: "support",
-    icon: "call",
-    title: "24/7 Support",
-    description: "Phone and Email support",
-  },
-];
 
 export const Values = () => {
   return (
